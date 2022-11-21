059b8e6a652b1750333f3c79e0bc6b7931b9a8c1
HungFai Koo <jhk888@yahoo.com>
2022-11-20 18:56:41 -0800

    refactor file for test cases

--- test/specs/uiControls.js
+++ test/specs/uiControls.js
@@ -1,14 +1,13 @@
-describe("ui Control", async () => {
+describe("UI Controls Test Suite", async () => {
   it("UI Controls", async () => {
     await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
     await $("input[name='username']").setValue("rahulshettyacademy");
     const password = $("//input[@type='password']");
     await password.setValue("learning");
-
     //what if multiple elements $$
-    const radioButtons = await $$(".customradio");
-    const userDropdown = radioButtons[1];
-    await userDropdown.$("span").click(); //chaining locators
+    const radionButtons = await $$(".customradio");
+    const userDropdown = radionButtons[1];
+    await userDropdown.$("span").click();
     await browser.pause(4000);
   });
 });

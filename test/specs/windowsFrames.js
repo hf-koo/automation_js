f8a5b85a5118f3d93bc56b87c8e24c8f81e59d0e
HungFai Koo <jhk888@yahoo.com>
2022-11-27 15:13:23 -0800

    switch back to new window and old window

--- test/specs/windowsFrames.js
+++ test/specs/windowsFrames.js
@@ -4,9 +4,16 @@ describe("Windows and Frames Miscellanous", async () => {
     await $(".blinkingText").click();
     const handles = await browser.getWindowHandles(); // 2 windows-
     await browser.switchToWindow(handles[1]);
-    console.log(await $("h1").getText());
+    console.log(await $("h1").getText()); // Rahul Shetty Academy
     await browser.closeWindow();
     await browser.switchToWindow(handles[0]);
-    console.log(await browser.getTitle());
+    console.log(await browser.getTitle()); //LoginPage Practise | Rahul Shetty
+    await browser.newWindow("https://google.com"); // automation
+    console.log(await browser.getTitle()); // Rahul Shetty Academy
+    await browser.switchWindow(
+      "https://rahulshettyacademy.com/loginpagePractise"
+    );
+    await $("#username").setValue("helloiswitchback");
+    await browser.pause(4000);
   });
 });

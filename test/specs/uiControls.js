a259229b7a3875833f65f400ec317f842cf59f0f
HungFai Koo <jhk888@yahoo.com>
2022-11-26 18:41:14 -0800

    testing dropdown menu

--- test/specs/uiControls.js
+++ test/specs/uiControls.js
@@ -14,5 +14,17 @@ describe("UI Controls Test Suite", async () => {
     await $("#cancelBtn").click();
     await $$(".customradio")[0].$("span").isSelected();
     await modal.waitForDisplayed();
+    await $("#okayBtn").click();
+    // validate pop up not shown up when you select admin
+
+    await $$(".customradio")[0].$("span").click();
+    await expect(modal).not.toBeDisplayed();
+    const dropdown = await $("select.form-control"); //select tag
+    await dropdown.selectByAttribute("value", "teach");
+    await dropdown.selectByVisibleText("Consultant");
+    await browser.pause(4000);
+
+    await dropdown.selectByIndex(0);
+    console.log(await dropdown.getValue());
   });
 });
